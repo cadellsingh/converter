00c9737c7041216f1f72ef1cce561d5392cad75c
Cadell Singh <cadellsingh@Cadells-MacBook-Pro.local>
2020-11-08 19:14:40 -0400

    n/a

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import { binaryToHex } from "./utils/binaryToHex";
-import { binaryToDecimal } from "./utils/binaryToDecimal";
-import { decimalToBinary } from "./utils/decimalToBinary";
-import { hexadecimalToDecimal } from "./utils/hexadecimalToDecimal";
+// import { binaryToHexadecimal } from "./utils/binaryToHexadecimal";
+// import { binaryToDecimal } from "./utils/binaryToDecimal";
+// import { decimalToBinary } from "./utils/decimalToBinary";
+// import { hexadecimalToDecimal } from "./utils/hexadecimalToDecimal";
 
 const App = () => {
-  const hexadecimal = "e7aa9";
-
-  const decimal = hexadecimalToDecimal(hexadecimal);
-
-  console.log(decimal);
+  // const hexadecimal = "e7aa9";
+  //
+  // const decimal = hexadecimalToDecimal(hexadecimal);
+  //
+  // console.log(decimal);
 
   return (
     <div>
