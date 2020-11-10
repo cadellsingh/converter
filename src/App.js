b8dfd4478e17dde1ecac9db4457fbabf969d6f06
Cadell Singh <cadellsingh9@gmail.com>
2020-11-09 20:37:35 -0400

    just added in some code to test inputs & validationg

--- src/App.js
+++ src/App.js
@@ -1,20 +1,75 @@
 import "./App.css";
-// import { binaryToHexadecimal } from "./utils/binaryToHexadecimal";
-// import { binaryToDecimal } from "./utils/binaryToDecimal";
-// import { decimalToBinary } from "./utils/decimalToBinary";
-// import { hexadecimalToDecimal } from "./utils/hexadecimalToDecimal";
+import React, { useState } from "react";
+import {
+  decimalValidation,
+  hexadecimalValidation,
+  binaryValidation,
+} from "./utils/validation";
 
 const App = () => {
-  // const hexadecimal = "e7aa9";
-  //
-  // const decimal = hexadecimalToDecimal(hexadecimal);
-  //
-  // console.log(decimal);
+  const [input, setInput] = useState({
+    binary: "",
+    hexadecimal: "",
+    decimal: "",
+  });
+
+  // prob same thing as above
+  const [valid, setValid] = useState(true);
+
+  // do validation in here - well maybe
+  const handleOnChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+
+    // prob switch statement
+    if (name === "hexadecimal") {
+      setValid(hexadecimalValidation(value));
+    } else if (name === "decimal") {
+      setValid(decimalValidation(value));
+    } else if (name === "binary") {
+      setValid(binaryValidation(value));
+    }
+  };
 
   return (
     <div>
-      <h1>test</h1>
-      <h1>more testong</h1>
+      <form>
+        {/* can extract into own component*/}
+        <label style={{ color: valid ? "black" : "red" }}>
+          Decimal
+          <input
+            type="text"
+            name="decimal"
+            value={input.decimal}
+            onChange={handleOnChange}
+          />
+        </label>
+
+        <label>
+          Hexadecimal
+          <input
+            type="text"
+            name="hexadecimal"
+            value={input.hexadecimal}
+            onChange={handleOnChange}
+          />
+        </label>
+
+        {/*<label>*/}
+        {/*  Binary*/}
+        {/*  <input*/}
+        {/*    type="text"*/}
+        {/*    name="binary"*/}
+        {/*    value={inputValue}*/}
+        {/*    onChange={handleOnChange}*/}
+        {/*  />*/}
+        {/*</label>*/}
+      </form>
     </div>
   );
 };
