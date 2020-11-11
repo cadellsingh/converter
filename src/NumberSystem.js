1b67dc91d3ec307ea68e4d96be444097ea2b64f3
Cadell Singh <cadellsingh9@gmail.com>
2020-11-11 18:26:53 -0400

    .

--- src/NumberSystem.js
+++ src/NumberSystem.js
@@ -1,36 +1,19 @@
 import React from "react";
-import {
-  binaryValidation,
-  decimalValidation,
-  hexadecimalValidation,
-} from "./utils/validation";
 
-const NumberSystem = ({ input, text, handleOnChange }) => {
-  const validation = (text) => {
-    return {
-      binary: binaryValidation(input),
-      decimal: decimalValidation(input),
-      hexadecimal: hexadecimalValidation(input),
-    }[text];
-  };
-
+const NumberSystem = ({ input, text, handleOnChange, valid }) => {
   const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  // if input is an empty string use that as a
-  // boolean value to disable form input
-
   return (
-    <label style={{ color: validation(text) ? "black" : "red" }}>
+    <label style={{ color: valid ? "black" : "red" }}>
       {capitalize(text)}
       <input
         type="text"
         name={text}
         value={input}
-        // placeholder="test"
         onChange={handleOnChange}
-        // disabled={input === ""}
+        // disabled={!valid}
       />
     </label>
   );
