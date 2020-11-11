6b3c3441aaa81dd95f4ba43ecb90e66627c58d30
Cadell Singh <cadellsingh9@gmail.com>
2020-11-10 21:42:14 -0400

    use input as boolean value to disable form input... so send whole object instead of key from parent component

--- src/NumberSystem.js
+++ src/NumberSystem.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { decimalValidation } from "./utils/validation";
 
 const NumberSystem = ({ input, valid, text, handleOnChange }) => {
+  // console.log(decimalValidation(input));
+
   const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
+
+  // if input is an empty string use that as a
+  // boolean value to disable form input
+
+  // prob move validation over here
+  // send input and destructure object to get name -> decimal / binary / hexa
+  // ^ it prob wont validate each letter
 
   return (
     <label style={{ color: valid ? "black" : "red" }}>
@@ -12,7 +22,7 @@ const NumberSystem = ({ input, valid, text, handleOnChange }) => {
         type="text"
         name={text}
         value={input}
-        placeholder="test"
+        // placeholder="test"
         onChange={handleOnChange}
       />
     </label>
