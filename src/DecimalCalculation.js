9334d41b78cab67f65d74e7592a0a7115e7385d3
Cadell Singh <cadellsingh9@gmail.com>
2020-11-16 20:33:33 -0400

    functions added for calculations;

--- src/DecimalCalculation.js
+++ src/DecimalCalculation.js
@@ -1,9 +1,69 @@
 import React from "react";
+import { binaryLetter } from "./utils/helpers";
 
-const DecimalCalculation = () => {
+const DecimalCalculation = ({ decimal }) => {
+  const decimalToBinaryCalc = (decimal) => {
+    let bitNum = 0;
+    let calculations = [];
+
+    while (decimal > 0) {
+      let quotient = Math.floor(decimal / 2);
+      let remainder = decimal % 2;
+
+      calculations.push(
+        <div>
+          <p>Division by 2: {decimal} / 2</p>
+          <p>Quotient: {quotient}</p>
+          <p>Remainder: {remainder}</p>
+          <p>Bit Number: {bitNum}</p>
+          <p>====</p>
+        </div>
+      );
+
+      bitNum++;
+      decimal = quotient;
+    }
+
+    return calculations;
+  };
+
+  const decimalToHexadecimalCalc = (decimal) => {
+    let bitNum = 0;
+    let calculations = [];
+
+    while (decimal > 0) {
+      let quotient = Math.floor(decimal / 16);
+      let remainder = decimal % 16;
+      let remainderHex = remainder > 9 ? binaryLetter(remainder) : remainder;
+
+      calculations.push(
+        <div>
+          <p>Division by 16: {decimal} / 16</p>
+          <p>Quotient: {quotient}</p>
+          <p>Remainder (decimal): {remainder}</p>
+          <p>Remainder (hexadecimal): {remainderHex}</p>
+          <p>Bit Number: {bitNum}</p>
+          <p>===</p>
+        </div>
+      );
+
+      bitNum++;
+      decimal = quotient;
+    }
+
+    return calculations;
+  };
+
   return (
-    <div className="calculation">
-      <h1>decimal</h1>
+    <div>
+      <div className="calculation">
+        <h1>decimal to binary Calc</h1>
+        {/*{decimalToBinaryCalc(decimal)}*/}
+      </div>
+      <div className="calculation">
+        <h1>decimal to hexadecimal Calc</h1>
+        {decimalToHexadecimalCalc(decimal)}
+      </div>
     </div>
   );
 };
--- src/HexadecimalCalculation.js
+++ src/HexadecimalCalculation.js
@@ -1,9 +1,49 @@
 import React from "react";
+import { binaryValue } from "./utils/helpers";
+import { decimalToBinary } from "./utils/hexadecimalToBinary";
+import { reverseString } from "./utils/helpers";
 
-const HexadecimalCalculation = () => {
+const HexadecimalCalculation = ({ hexadecimal }) => {
+  const hexadecimalToBinaryCalc = (hexadecimal) => {
+    return hexadecimal.split("").map((alpha) => {
+      let hex = isNaN(alpha) ? binaryValue(alpha) : alpha;
+      let binary = decimalToBinary(hex);
+
+      return (
+        <p>
+          {alpha}: {binary}
+        </p>
+      );
+    });
+  };
+
+  const hexadecimalToDecimalCalc = (hexadecimal) => {
+    let hexaArray = reverseString(hexadecimal)
+      .split("")
+      .map((alpha) => {
+        return isNaN(alpha) ? binaryValue(alpha) : parseInt(alpha);
+      });
+
+    return hexaArray.map((digit, index) => {
+      let sum = digit * Math.pow(16, index);
+      return (
+        <p>
+          {digit} * 16 ^ {index}: {sum}
+        </p>
+      );
+    });
+  };
+
   return (
-    <div className="calculation">
-      <h1>hexadecimal</h1>
+    <div>
+      <div className="calculation">
+        <h1>hexadecimal to binary calc</h1>
+        {hexadecimalToBinaryCalc(hexadecimal)}
+      </div>
+      <div className="calculation">
+        <h1>hexadecimal to decimal calc</h1>
+        {hexadecimalToDecimalCalc(hexadecimal)}
+      </div>
     </div>
   );
 };
