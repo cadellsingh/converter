4c0840a57095707a79aeba17bdc992f6084be8a4
Cadell Singh <cadellsingh9@gmail.com>
2020-11-18 13:50:10 -0400

    component to hold calculation components;

---
+++ src/CalculationRow.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CalculationRow = ({ input }) => {
+  return (
+    <div>
+      <h1>hello</h1>
+
+      {/* if decimal is being focused then do decimal calculation and so forth*/}
+      {/*<BinaryCalculation binary={binary} />*/}
+      {/*<HexadecimalCalculation hexadecimal={hexadecimal} />*/}
+      {/*<DecimalCalculation decimal={decimal} />*/}
+    </div>
+  );
+};
+
+export default CalculationRow;
