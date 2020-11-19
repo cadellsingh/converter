d821643faa4b7b4df51b33fa3fd96fa620f02158
Cadell Singh <cadellsingh9@gmail.com>
2020-11-19 10:14:13 -0400

    .

--- src/CalculationRow.js
+++ src/CalculationRow.js
@@ -3,19 +3,14 @@ import BinaryCalculation from "./BinaryCalculations";
 import HexadecimalCalculation from "./HexadecimalCalculation";
 import DecimalCalculation from "./DecimalCalculation";
 
-// may have to switch up certain methods
-// for example binaryCalculation might have decimalToBinary & hexadecimalToBinary
-// same for the other methods
-// gonna have to pass two number systems to each calculation component
-
 const CalculationRow = ({ input }) => {
   const { decimal, hexadecimal, binary } = input;
 
   return (
     <div className="container">
-      <BinaryCalculation binary={binary} />
-      <HexadecimalCalculation hexadecimal={hexadecimal} />
-      <DecimalCalculation decimal={decimal} />
+      <DecimalCalculation binary={binary} hexadecimal={hexadecimal} />
+      <BinaryCalculation decimal={decimal} hexadecimal={hexadecimal} />
+      <HexadecimalCalculation decimal={decimal} binary={binary} />
     </div>
   );
 };
